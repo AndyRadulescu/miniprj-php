5d9047fa366a18fc0a2488dcc70f50217de58ec0
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2018-08-25 13:38:54 +0300

    added login functionality with sesions

--- classes/Model.php
+++ classes/Model.php
@@ -58,4 +58,10 @@ abstract class Model
     {
         return $this->dbh->lastInsertId();
     }
+
+    public function single()
+    {
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }
